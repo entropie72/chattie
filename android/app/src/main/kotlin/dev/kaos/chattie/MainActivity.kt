277c74f4635c80cc114bcb6c9220364bcda7feab
Nguyen Nhat Hao <nguyennhathao@yandex.com>
2022-06-08 16:23:34 +0700

    feat: Initial commit

---
+++ android/app/src/main/kotlin/dev/kaos/chattie/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.kaos.chattie
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MaterialApp(
+    home: App(),
+  ));
+}
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('This is some text'),
+      ),
+    );
+  }
+}
